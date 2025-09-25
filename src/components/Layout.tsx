63e548da08105a34caa4b3bb6663113566de54d2
Balu Babu <balub997@gmail.com>
2025-09-25 16:54:05 +0530

    chore: removed the side bar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="flex min-h-screen bg-background">
-      <Sidebar />
-      <main className="flex-1 overflow-auto">
+    <div className="min-h-screen bg-background">
+      <main className="overflow-auto">
         <Outlet />
       </main>
     </div>
